09d5f583d3aaa20629b786cce39467617357b24b
tahid02 <tanzim1463@gmail.com>
2021-05-23 06:43:24 +0600

    service and edit updated

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
-const cors= require('cors')
+const cors = require('cors')
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 const { ObjectID } = require('mongodb')
+// "start": "node index.js",
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z6ers.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express()
-const port =  process.env.PORT || 5000
+const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
@@ -16,11 +18,11 @@ client.connect(err => {
   const servicesCollection = client.db("carRentalDB").collection("services");
   const rentCollection = client.db("carRentalDB").collection("rents");
   const adminsCollection = client.db("carRentalDB").collection("admins");
-  const reviewsCollection= client.db("carRentalDB").collection("reviews");
+  const reviewsCollection = client.db("carRentalDB").collection("reviews");
   console.log('database connected')
   // perform actions on the collection object
 
-   app.post('/addServices', (req, res) => {
+  app.post('/addServices', (req, res) => {
     const newServices = req.body;
     // console.log('adding new services: ', newServices)
     servicesCollection.insertOne(newServices)
@@ -31,11 +33,11 @@ client.connect(err => {
       })
   })
 
-    app.get('/services', (req, res) => {
+  app.get('/services', (req, res) => {
 
     servicesCollection.find({})
       .toArray((err, services) => {
-        
+
         res.send(services)
       })
   })
@@ -45,7 +47,7 @@ client.connect(err => {
 
     const id = ObjectID(req.params.id)
     // console.log(id)
-    servicesCollection.find({ _id: id }).toArray((err, documents) =>  {
+    servicesCollection.find({ _id: id }).toArray((err, documents) => {
       res.send(documents[0])
     })
 
@@ -62,19 +64,19 @@ client.connect(err => {
         res.send(result.insertedCount > 0)
 
       })
-      .catch(err => console.log(err ))
+      .catch(err => console.log(err))
   })
 
-  
 
-  app.get('/userRents', (req,res) => {
+
+  app.get('/userRents', (req, res) => {
     const queryEmail = req.query.email;
-    console.log('user Rents',queryEmail)
-    rentCollection.find( {email: queryEmail} )
-    .toArray( (err,documents) => {
-      console.log(documents)
-      res.send(documents)
-    })
+    console.log('user Rents', queryEmail)
+    rentCollection.find({ email: queryEmail })
+      .toArray((err, documents) => {
+        console.log(documents)
+        res.send(documents)
+      })
   })
 
 
@@ -100,70 +102,124 @@ client.connect(err => {
         res.send(result.insertedCount > 0)
 
       })
-      .catch(err => console.log(err ))
+      .catch(err => console.log(err))
   })
 
 
 
 
 
-    app.post('/isAdmin', (req, res) => {
-        const email = req.body.email;
-        console.log('admin checking email', email)
-        adminsCollection.find({ email: email })
-            .toArray((err, admins) => {
-              console.log(admins)
-                res.send(admins.length > 0);
-            })
-    })
+  app.post('/isAdmin', (req, res) => {
+    const email = req.body.email;
+    console.log('admin checking email', email)
+    adminsCollection.find({ email: email })
+      .toArray((err, admins) => {
+        console.log(admins)
+        res.send(admins.length > 0);
+      })
+  })
 
 
 
 
-    app.post('/addReviews', (req, res) => {
-      const newReviews = req.body;
-      console.log('new Reviews', newReviews)
-      reviewsCollection.insertOne(newReviews)
-        .then(result => {
-          // console.log('inserted count', result.insertedCount);
-          res.send(result.insertedCount > 0)
-  
-        })
-        .catch(err => console.log(err ))
-    })
-
-
-
-    app.get('/reviews', (req, res) => {
-
-      reviewsCollection.find({})
-        .toArray((err, reviews) => {
-          console.log(reviews)
-          res.send(reviews)
-        })
-    })
-
-    app.delete('/delete/:id',(req,res) =>{
-      servicesCollection.deleteOne({ _id: ObjectID(req.params.id)})
+  app.post('/addReviews', (req, res) => {
+    const newReviews = req.body;
+    console.log('new Reviews', newReviews)
+    reviewsCollection.insertOne(newReviews)
       .then(result => {
-        console.log( 'deleted', result.deletedCount )
+        // console.log('inserted count', result.insertedCount);
+        res.send(result.insertedCount > 0)
+
+      })
+      .catch(err => console.log(err))
+  })
+
+
+
+  app.get('/reviews', (req, res) => {
+
+    reviewsCollection.find({})
+      .toArray((err, reviews) => {
+        console.log(reviews)
+        res.send(reviews)
+      })
+  })
+
+  app.delete('/delete/:id', (req, res) => {
+    servicesCollection.deleteOne({ _id: ObjectID(req.params.id) })
+      .then(result => {
+        console.log('deleted', result.deletedCount)
         res.send(result.deletedCount > 0)
-        })
-    })
+      })
+  })
+
+
+
+  app.patch('/update/:id', (req, res) => {
+    console.log(req.body);
+    const { type, price, description, imageURL } = req.body
+
+    servicesCollection.updateOne(
+      { _id: ObjectID(req.params.id) },
+      {
+        $set: { type, price, description, imageURL }
+
+      }
+    )
+      .then(result => {
+        console.log(result);
+        res.send(result.modifiedCount > 0)
+      })
+      .catch(err => console.log(err))
+
+  })
+
+
+  app.patch('/statusUpdate/:id', (req, res) => {
+    console.log(req.body);
+    // console.log(req.params.id);
+    
+
+
+    rentCollection.updateOne(
+      { _id: ObjectID(req.params.id) },
+      {
+        $set: { rentStatus:req.body.status }
+      }
+    )
+      .then(result => {
+        console.log(result);
+        res.send(result.modifiedCount > 0)
+      })
+      .catch(err => console.log(err))
+
+  })
 
 
 
 
+//adding rentStatus field with the value of 'Pending' in all documents of rentCollection 
+  // rentCollection.updateMany({}, { $set: { "rentStatus": "Pending" } }).then(result => {
+  //   console.log(result);
+  // })
+  //   .catch(err => console.log(err))
+
+
+// removing isOpen field from rentCollection those documents have rentStatus field
+  // rentCollection.updateMany(
+  //   { rentStatus: "Pending" },
+  //   {
+  //     $unset: { isOpen: false }
+  //   }
+  // ).then(result => {
+  //   console.log(result);
+  // })
+  //   .catch(err => console.log(err))
 
 
 
 
-
-
-
-
-
-//   client.close();
+  //   client.close();
 });
 
 
